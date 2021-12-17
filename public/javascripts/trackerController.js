7551f0234a48af53b8bba2178272b56b22a8b3af
Shane Porter <sporter@salesforce.com>
2021-12-17 16:52:01 +0000

    Implemented UUID and the express endpoint for game page

---
+++ public/javascripts/trackerController.js
@@ -0,0 +1,9 @@
+console.log('Hello World!');
+
+
+const queryString = window.location.search;
+console.log(queryString);
+const urlParams = new URLSearchParams(queryString);
+
+const gameId = urlParams.get('gameId')
+console.log(gameId);
---
+++ routes/game.js
@@ -0,0 +1,26 @@
+const { 
+    v1: uuidv1,
+    v4: uuidv4,
+  } = require('uuid');
+  
+var express = require('express');
+var path = require('path');
+var router = express.Router();
+
+/* GET game tracker page. */
+router.get('/', function(req, res, next) {
+    res.sendFile(path.join(__dirname+'/tracker.html'));
+    console.log(req._parsedOriginalUrl.query);
+    if(req._parsedOriginalUrl.query == null){
+        //generate a unique id based on timestamp uuidv4 would be a unique id that's random
+        const gameId1 = uuidv1();
+        console.log(gameId1); // -> '6c84fb90-12c4-11e1-840d-7b25c5ee775a'   
+        res.redirect('/game?gameId='+gameId1);
+        console.log(req);
+    } else if(req._parsedOriginalUrl.query != null) {
+        console.log(req._parsedOriginalUrl.query);
+    };
+});
+
+
+module.exports = router;
