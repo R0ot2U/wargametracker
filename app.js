040cdbd5ff8e6242d2b87d776f775eb46b0963bd
Shane Porter <sporter@salesforce.com>
2021-12-18 01:33:41 +0000

    db writing tests

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var gameRouter = require('./routes/game');
@@ -13,7 +12,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- routes/game.js
+++ routes/game.js
@@ -2,6 +2,15 @@ const {
     v1: uuidv1,
     v4: uuidv4,
   } = require('uuid');
+
+//db
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
   
 var express = require('express');
 var path = require('path');
@@ -13,9 +22,16 @@ router.get('/', function(req, res, next) {
     console.log(req._parsedOriginalUrl.query);
     if(req._parsedOriginalUrl.query == null){
         //generate a unique id based on timestamp uuidv4 would be a unique id that's random
-        const gameId1 = uuidv1();
-        console.log(gameId1); // -> '6c84fb90-12c4-11e1-840d-7b25c5ee775a'   
-        res.redirect('/game?gameId='+gameId1);
+        const gameId = uuidv1();
+        console.log(gameId); // -> '6c84fb90-12c4-11e1-840d-7b25c5ee775a' 
+        pool.query(
+            "INSERT INTO games(game_id)VALUES('"+gameId+"')",
+            (err, res) => {
+              console.log(err, res);
+              pool.end();
+            }
+          );  
+        res.redirect('/game?gameId='+gameId);
     } else if(req._parsedOriginalUrl.query != null) {
         console.log(req._parsedOriginalUrl.query);
     };
