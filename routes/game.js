a7cdd08fe730b9ba9a19b009b57cbcb83913fb69
Shane Porter <sporter@salesforce.com>
2021-12-20 16:02:45 +0000

    db async query

--- routes/game.js
+++ routes/game.js
@@ -32,25 +32,30 @@ var router = express.Router();
 
 /* GET game tracker page. */
 router.get('/', async function(req, res, next) {
-    res.sendFile(path.join(__dirname+'/tracker.html'));
-    console.log(req._parsedOriginalUrl.query);
-    if(req._parsedOriginalUrl.query == null){
-        //generate a unique id based on timestamp uuidv4 would be a unique id that's random
-        const gameId = uuidv1();
-        console.log(gameId); // -> '6c84fb90-12c4-11e1-840d-7b25c5ee775a' 
-        pool.query(
-            "INSERT INTO games(game_id)VALUES('"+gameId+"')",
-            (err, res) => {
-              console.log(err, res);
-              pool.end();
-            }
-          );  
-        res.redirect('/game?gameId='+gameId);
-    } else if(req._parsedOriginalUrl.query != null) {
+    try {
+        res.sendFile(path.join(__dirname+'/tracker.html'));
         console.log(req._parsedOriginalUrl.query);
-        var results2 = await asyncDB();
-        res.send(results2);
-    };
+        if(req._parsedOriginalUrl.query == null){
+            //generate a unique id based on timestamp uuidv4 would be a unique id that's random
+            const gameId = uuidv1();
+            console.log(gameId); // -> '6c84fb90-12c4-11e1-840d-7b25c5ee775a' 
+            pool.query(
+                "INSERT INTO games(game_id)VALUES('"+gameId+"')",
+                (err, res) => {
+                console.log(err, res);
+                pool.end();
+                }
+            );  
+            res.redirect('/game?gameId='+gameId);
+        } else if(req._parsedOriginalUrl.query != null) {
+            console.log(req._parsedOriginalUrl.query);
+            var results2 = await asyncDB();
+            res.send(results2);
+        };
+    } catch (err) {
+        console.error(err);
+        res.send("Error " + err);
+    }
 });
 
 
