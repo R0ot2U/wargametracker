3bd76c3031c92dd70e96a68c1fc212b29f16c364
Shane Porter <sporter@salesforce.com>
2021-12-20 17:46:03 +0000

    db async query

--- public/javascripts/trackerController.js
+++ public/javascripts/trackerController.js
@@ -1,8 +1,11 @@
 console.log('Hello World!');
+console.log(gameIdToSet);
 
 const queryString = window.location.search;
 console.log(queryString);
 const urlParams = new URLSearchParams(queryString);
+
+urlParams.append('gameId', {gameIdToSet});
 
 const gameId = urlParams.get('gameId');
 console.log(gameId);
