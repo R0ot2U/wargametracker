72c1b862f2f3e30cb006d160dda5dec18fe3850d
Shane Porter <sporter@salesforce.com>
2021-12-22 22:14:10 +0000

    testing out loading json in pages

--- routes/game.js
+++ routes/game.js
@@ -49,7 +49,7 @@ router.get('/', async function(req, res, next) {
         } else if(req._parsedOriginalUrl.query != null) {
             console.log(req._parsedOriginalUrl.query);
             var results2 = await asyncDB(req.query.gameId);
-            console.log(results2.rows.length);
+            console.log(results2.rows.length());
             if(results2.rows.length==1) {
                 var gameData = {
                     "game_id": results[0].gameId,
